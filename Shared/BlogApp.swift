2d45ff72ea5ddc8a863b9d14df5a5b6e64fef1ee
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-08-22 16:39:12 +0200

    Initial Commit

---
+++ Shared/BlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 22/08/21.
+//
+
+import SwiftUI
+
+@main
+struct BlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
